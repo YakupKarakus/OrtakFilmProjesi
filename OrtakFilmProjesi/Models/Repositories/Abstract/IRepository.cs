f14e5e40a12440e11c101b2a48af0749bafb1288
EmRe-ozp <99139469+EmRe-ozp@users.noreply.github.com>
2022-05-26 15:20:28 +0300

    repository eklendi

---
+++ OrtakFilmProjesi/Models/Repositories/Abstract/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrtakFilmProjesi.Models.Repositories.Abstract
+{
+    public interface IRepository<T> where T:class
+    {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        bool Add(T entity);
+        bool Update(T entity);
+        bool Delete(T entity);
+    }
+}
---
+++ OrtakFilmProjesi/Models/Repositories/Concrete/Genericrepository.cs
@@ -0,0 +1,47 @@
+﻿using OrtakFilmProjesi.Models.Database;
+using OrtakFilmProjesi.Models.Repositories.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrtakFilmProjesi.Models.Repositories.Concrete
+{
+    public class Genericrepository<T> : IRepository<T> where T : BaseEntity
+    {
+        private readonly ApplicationDbContext db;
+
+        public Genericrepository(ApplicationDbContext _db)
+        {
+            this.db = _db;
+        }
+
+        public bool Add(T entity)
+        {
+            db.Set<T>().Add(entity);
+            return db.SaveChanges() > 0;
+        }
+
+        public bool Delete(T entity)
+        {
+            db.Set<T>().Remove(entity);
+            return db.SaveChanges() > 0;
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return db.Set<T>();
+        }
+
+        public T GetById(int id)
+        {
+            return db.Set<T>().FirstOrDefault(a => a.Id == id);
+        }
+
+        public bool Update(T entity)
+        {
+            db.Set<T>().Update(entity);
+            return db.SaveChanges() > 0;
+        }
+    }
+}
