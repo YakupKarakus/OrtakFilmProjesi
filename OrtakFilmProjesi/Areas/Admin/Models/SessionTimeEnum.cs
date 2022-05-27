7e792058a9864de6e12e8f4637a446efb77e44cd
Ersen Ozen <ersenozen@outlook.com>
2022-05-27 11:51:11 +0300

    Session page for admin created

---
+++ OrtakFilmProjesi/Areas/Admin/Models/SessionTimeEnum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrtakFilmProjesi.Areas.Admin.Models
+{
+    public enum SessionTimeEnum
+    {
+        A = 11,
+        B = 12,
+        C = 13,
+        D = 14,
+        E = 15,
+        F = 16,
+        G = 17,
+        H = 18,
+        I = 19,
+        J = 20,
+        K = 21,
+        L = 22,
+        M = 23,
+        N = 24
+    }
+}
---
+++ OrtakFilmProjesi/Areas/Admin/Models/SessionVM.cs
@@ -0,0 +1,14 @@
+﻿using OrtakFilmProjesi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrtakFilmProjesi.Areas.Admin.Models
+{    
+    public class SessionVM
+    {
+        public Session Session { get; set; }
+        
+    }
+}
